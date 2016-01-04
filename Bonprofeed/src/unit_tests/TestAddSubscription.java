cd7cf8ad08ea50c471bbaaac64293badb7be1b83
DavidCatalan <freedavid94@gmail.com>
2016-01-04 11:13:54 +0100

    Test insertFeed

--- Bonprofeed/src/unit_tests/TestAddSubscription.java
+++ Bonprofeed/src/unit_tests/TestAddSubscription.java
@@ -4,11 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import model.DatabaseHandler;
+
 public class TestAddSubscription {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void add_New_Subscription_to_DB() {
+		
+		// Arrange
+		String url = "http://feeds.feedburner.com/FluProject";
+		String name = "Flu Project";
+		
+		DatabaseHandler dbh = new DatabaseHandler();
+		
+		int inserted = dbh.insertFeed( url, name );
+		
+		assertEquals( inserted, 1 );
+	
 	}
-
+	
+	@Test
+	public void add_Same_Subscription_to_DB() {
+		
+	}
+	
 }
