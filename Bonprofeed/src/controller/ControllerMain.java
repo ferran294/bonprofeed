b0dce05df84e8fcb86cec1e4017d821b397e01a8
Ferran <ferran294@gmail.com>
2016-01-19 18:31:03 +0100

    Nueva carpeta,nuevo Feed,Window Loader class

--- Bonprofeed/src/controller/ControllerMain.java
+++ Bonprofeed/src/controller/ControllerMain.java
@@ -20,8 +20,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
@@ -37,15 +40,22 @@ import model.DatabaseHandler;
 import model.Feed;
 import model.Folder;
 import model.RomeOperations;
+import model.Tag;
 
 public class ControllerMain implements Initializable{
 	
 	private DatabaseHandler dbh = new DatabaseHandler();
 	private RomeOperations rome;
-	private static String actualView;
+	private WindowLoader windowLoader = new WindowLoader();
 	private static Folder actualFolder;
+	private static Tag actualTag;
 	ArrayList<Folder> folders = dbh.getFolders();
 	@FXML TreeView<String> folderTree;
+	@FXML TreeView<String> tagTree;
+	//@FXML
+	//private TableView<Article> articlesList;
+	//@FXML
+	//private TableColumn<Article, String> columnTitle;
 	@FXML private AnchorPane panelArticles;
 	//Folder view
 	@FXML
@@ -58,6 +68,25 @@ public class ControllerMain implements Initializable{
 	private Label errorFolder;
 	@FXML
 	private Button btnDeleteFolder;
+	@FXML
+	private Button btnBack;
+	//New Folder view
+	@FXML
+	private TextField textNewFolder;
+	@FXML
+	private Button btnNewFolder;
+	@FXML
+	private Label errorNewFolder;
+	//New Tag
+	@FXML
+	private Button btnNewTag;
+	@FXML
+	private TextField textNewTag;
+	@FXML
+	private Label textErrorTag;
+	//Tag
+	@FXML
+	private Label labelTagName;
 	
 	public ControllerMain(){
 		super();
@@ -65,15 +94,28 @@ public class ControllerMain implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		String actualView = windowLoader.getActualView();
 		
-		if(actualView == "main"){
-			generateFolderTree();
-		}else{
+		if(actualView == "folder"){
 			labelFolderName.setText(actualFolder.getName());
+		}else if (actualView == "newFolder"){
+			
+		}else if(actualView == "newTag"){
+			
+		}else if(actualView == "tag"){
+			labelTagName.setText(actualTag.getName());
+		}else{
+			generateFolderTree();
+			generateTagTree();
 		}
 			
 		
 		
+	}
+	
+	public void generateArticleList(){
+		dbh = new DatabaseHandler();
+		//ArrayList<Article> articles = dbh.getArticles();
 	}
 	
 	public void generateFolderTree(){
@@ -135,15 +177,82 @@ public class ControllerMain implements Initializable{
 		
  	}
 	
-	public void newFeed(ActionEvent event) throws IOException{
-		Parent parent = FXMLLoader.load(getClass().getResource("/view/NewFeed.fxml"));
-		Stage stage = new Stage();
-		Scene scene = new Scene(parent);
+	public void generateTagTree(){
+ 		//TreeFolder
+		dbh = new DatabaseHandler();
+ 		TreeItem<String> rootNode = new TreeItem<String>("Todas las etiquetas");
+ 		ArrayList<Tag> tags = dbh.getTags();
+ 		
+		rootNode.setExpanded(true);
 		
-		stage.setResizable(false);
-		stage.setScene(scene);
-		stage.setTitle("Añade un nuevo Feed");
-		stage.show();
+		
+		for (Tag tag : tags) {
+            TreeItem<String> foldLeaf = new TreeItem<String>(tag.getName());
+            rootNode.getChildren().add(foldLeaf);
+           
+            ArrayList<Feed> feeds = tag.getFeeds();
+            
+            if(feeds.isEmpty()){
+            	TreeItem<String> feedLeaf = new TreeItem<String>("No hay Feeds");
+            	foldLeaf.getChildren().add(feedLeaf);
+            }else{
+            	for(Feed feed : feeds){
+                	TreeItem<String> feedLeaf = new TreeItem<String>(feed.getName());
+                	foldLeaf.getChildren().add(feedLeaf);
+                }
+            }
+            
+  
+	    }
+		
+		
+		
+		tagTree.setRoot(rootNode);
+		
+		tagTree.setEditable(true);
+		tagTree.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+	    tagTree.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+	            
+        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            
+            TreeItem treeItem = (TreeItem)newValue;
+            
+            if(!treeItem.isLeaf() && treeItem.getParent() != null){
+            	
+            	Tag tagSelected = getTag(tags, treeItem.getValue().toString());
+    			loadTagWindow(tagSelected, treeItem);
+            }
+            
+    		
+    		               	                	                
+            
+        }
+
+		private Tag getTag(ArrayList<Tag> tags, String tagName) {
+			for(Tag t : tags){
+		        if(t.getName() == tagName){
+		        	return t;
+		        }
+		    }
+			return null;
+			
+		}
+            
+	    });
+		
+ 	}
+	
+	public void loadTagWindow(Tag tag,TreeItem treeItem){
+		actualTag = tag;
+		windowLoader.loadTagWindow(tag, treeItem,tagTree);
+		 
+	}
+	
+	public void loadNewFeedWindow(ActionEvent event) throws IOException{
+		
+		windowLoader.loadNewFeedWindow(event, this.folderTree);
+		
+		 
 	}
 	
 	private Folder getFolder(ArrayList<Folder> folderList,String folderName){
@@ -157,11 +266,9 @@ public class ControllerMain implements Initializable{
 		
 	}
 	
-	public static void setActualView(String newView){
-		actualView = newView;
-	}
 	
-	//Folder Window 
+	
+	//Folder Window -------------------------------
 	
 	
 	
@@ -186,57 +293,93 @@ public class ControllerMain implements Initializable{
 		dbh = new DatabaseHandler();
 		String folderName = labelFolderName.getText();
 		dbh.deleteFolder(folderName);
-		
-		loadMain(btnDeleteFolder);
+		windowLoader.loadMain(btnDeleteFolder);
 	}
 	
+	public void backFromFolder(){
+		windowLoader.loadMain(btnBack);
+	}
+	
+	
 	public void loadFolderWindow(Folder folder,TreeItem treeItem){
-		Stage stage;
-		Parent root;
-		
-		
-		try {
-			setActualView("folder");
-			actualFolder = folder;
-			stage = (Stage) this.folderTree.getScene().getWindow();
-			root = FXMLLoader.load(getClass().getResource("/view/Folder.fxml"));
-			Scene scene = new Scene(root);
-			stage.setTitle(treeItem.getValue().toString());
-		    stage.setScene(scene);
-		    
-		    stage.show();
-		    
-		    
-	        System.out.println("Selected item is" + treeItem);	
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
+		actualFolder = folder;
+		windowLoader.loadFolderWindow(folder, treeItem,folderTree);
 		 
 	}
 	
 	public void loadMain(Node element){
-		Stage stage;
-		Parent root;
+		windowLoader.loadMain(element);
+	}
+	
+	// ---------------------------------------------------
+	
+	
+	//------- New Folder Controller -----------
+	
+	public void loadNewFolder(){
+		windowLoader.loadNewFolder(btnNewFolder);
+	}
+	
+	public void createFolder(){
+		dbh = new DatabaseHandler();
+		String newFolderName = textNewFolder.getText();
 		
 		
-		try {
-			setActualView("main");
-			stage = (Stage) element.getScene().getWindow();
-			root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
-			Scene scene = new Scene(root);
-			stage.setTitle("BonProfeed");
-		    stage.setScene(scene);
-		    stage.show();
-		    
-		    
-		} catch (IOException e) {
-			e.printStackTrace();
+		int done = dbh.createFolder(newFolderName);
+		
+		if(done == 0){
+			
+			errorNewFolder.setText("El nombre de carpeta ya existe");
+		}else{
+			
+			
+			loadMain(textNewFolder);
 		}
 	}
 	
+	// --------- New Tag Controller ----------------
 	
-
+	public void loadNewTagWindow(){
+		windowLoader.loadNewTag(btnNewTag);
+	}
 	
+	public void addTag(){
+		dbh = new DatabaseHandler();
+		String newTagName = textNewTag.getText();
+		
+		int done = dbh.createTag(newTagName);
+		
+		if(done == 0){
+			textErrorTag.setText("El nombre de etiqueta ya existe");
+		}else{
+			loadMain(textNewTag);
+		}
+	}
+	
+	// ----------------- Tag Controller ---------
+	public void setTagName(){
+		/*
+		dbh = new DatabaseHandler();
+		String newTagName = textNewTag.getText();
+		String tagName = labelTagName.getText();
+		System.out.println(newTagName);
+		System.out.println(tagName);
+		
+		int done = dbh.renameTag(tagName, newTagName);
+		
+		if(done == 0){
+			errorFolder.setText("El nombre de etiqueta ya existe");
+		}else{
+			labelTagName.setText(newTagName);
+			errorFolder.setText("Hecho!");
+		}
+		*/
+	}
+	
+	public void deleteTag(){
+		dbh = new DatabaseHandler();
+		String tagName = labelTagName.getText();
+		dbh.deleteTag(tagName);
+		windowLoader.loadMain(labelTagName);
+	}
 }
