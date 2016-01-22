e3a0e4196b0ebf30f4fa39b2170637032f779b22
Ferran <ferran294@gmail.com>
2016-01-22 19:45:20 +0100

    ver Feeds y Articles en Folder y Tag

--- Bonprofeed/src/controller/ControllerMain.java
+++ Bonprofeed/src/controller/ControllerMain.java
@@ -110,6 +110,16 @@ public class ControllerMain implements Initializable{
 	private ListView listFeeds;
 	@FXML
 	private TextField feedNameDis;
+	@FXML
+	private TableView<Article> articlesListFolder;
+	@FXML
+	private TableColumn<Article, Integer> columnVistoFolder;
+	@FXML
+	private TableColumn<Article, String> columnTitleFolder;
+	@FXML
+	private TableColumn<Article, String> columnAuthorFolder;
+	@FXML
+	private TableColumn<Article, Date> columnDateFolder;
 	//New Folder view
 	@FXML
 	private TextField textNewFolder;
@@ -131,6 +141,18 @@ public class ControllerMain implements Initializable{
 	private TextField feedDisTag;
 	@FXML
 	private Label errorDisTag;
+	@FXML
+	private ListView listFeedsTag;
+	@FXML
+	private TableView<Article> articlesListTag;
+	@FXML
+	private TableColumn<Article, Integer> columnVistoTag;
+	@FXML
+	private TableColumn<Article, String> columnTitleTag;
+	@FXML
+	private TableColumn<Article, String> columnAuthorTag;
+	@FXML
+	private TableColumn<Article, Date> columnDateTag;
 	//Article
 	@FXML
 	private Label textTitle;
@@ -138,8 +160,6 @@ public class ControllerMain implements Initializable{
 	private Label textAuthor;
 	@FXML
 	private Label textDate;
-	@FXML
-	private ListView listFeedsTag;
 	@FXML private WebView htmlContainer = new WebView();
 	//Assign
 	@FXML
@@ -174,6 +194,11 @@ public class ControllerMain implements Initializable{
 			FeedListFactory listGenerator = new FeedListFactory(dbh, listFeeds);
 			listGenerator.generateFolderFeedsList(actualFolder.getName());
 			
+			ObservableList<Article> articles = FXCollections.observableArrayList(dbh.getArticlesFromFolder(actualFolder.getName()));
+			ArticleTable articleTable = new ArticleTable(articlesListFolder, columnVistoFolder, columnTitleFolder, columnAuthorFolder, columnDateFolder);
+			ArticleTableLoader tableLoader = new ArticleTableLoader(articleTable, articles, windowLoader);
+			tableLoader.generateArticleTable();
+			
 		}else if (actualView == "newFolder"){
 			
 		}else if(actualView == "newTag"){
@@ -183,6 +208,11 @@ public class ControllerMain implements Initializable{
 			labelTagName.setText(actualTag.getName());
 			FeedListFactory listGenerator = new FeedListFactory(dbh, listFeedsTag);
 			listGenerator.generateTagFeedsList(actualTag.getName());
+			
+			ObservableList<Article> articles = FXCollections.observableArrayList(dbh.getArticlesFromTag(actualTag.getName()));
+			ArticleTable articleTable = new ArticleTable(articlesListTag, columnVistoTag, columnTitleTag, columnAuthorTag, columnDateTag);
+			ArticleTableLoader tableLoader = new ArticleTableLoader(articleTable, articles, windowLoader);
+			tableLoader.generateArticleTable();
 			
 		}else if(actualView == "article"){
 			
